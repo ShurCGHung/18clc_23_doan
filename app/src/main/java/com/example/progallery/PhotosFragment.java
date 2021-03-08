b792de7921ccb53ee1068e43dce04d75c049717d
Where is Neo? <alec.bui.vhp@gmail.com>
2021-03-09 01:10:34 +0700

    (+) Grid view + Adapter
    For displaying all photos in photo tab

--- app/src/main/java/com/example/progallery/PhotosFragment.java
+++ app/src/main/java/com/example/progallery/PhotosFragment.java
@@ -7,6 +7,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.GridView;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -23,6 +24,11 @@ public class PhotosFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+    GridView gridView;
+    int[] photos = {
+            R.drawable.photo, R.drawable.photo, R.drawable.photo, R.drawable.photo, R.drawable.photo, R.drawable.photo, R.drawable.photo, R.drawable.photo, R.drawable.photo, R.drawable.photo, R.drawable.photo, R.drawable.photo, R.drawable.photo, R.drawable.photo, R.drawable.photo, R.drawable.photo, R.drawable.photo, R.drawable.photo, R.drawable.photo, R.drawable.photo, R.drawable.photo, R.drawable.photo, R.drawable.photo, R.drawable.photo, R.drawable.photo,
+    };
 
     public PhotosFragment() {
         // Required empty public constructor
@@ -59,6 +65,13 @@ public class PhotosFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_photos, container, false);
+        View view = inflater.inflate(R.layout.fragment_photos, container, false);
+
+        gridView = (GridView) view.findViewById(R.id.grid_view);
+
+        PhotoAdapter photoAdapter = new PhotoAdapter(getActivity().getApplicationContext(), photos);
+        gridView.setAdapter(photoAdapter);
+
+        return view;
     }
 }
