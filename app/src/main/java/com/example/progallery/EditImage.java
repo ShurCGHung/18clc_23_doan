769c5d2fb827d34252dd3b490bda2426672d8474
Hưng Cao <caogiahung11A2@gmail.com>
2021-03-09 12:34:38 +0700

    (+): Add java class and layout
    - Add ViewImage and EditImage class
    - Add edit_image layout

---
+++ app/src/main/java/com/example/progallery/EditImage.java
@@ -0,0 +1,4 @@
+package com.example.progallery;
+
+public class EditImage {
+}
---
+++ app/src/main/java/com/example/progallery/ViewImage.java
@@ -0,0 +1,7 @@
+package com.example.progallery;
+
+import android.app.Activity;
+
+public class ViewImage extends Activity {
+
+}
