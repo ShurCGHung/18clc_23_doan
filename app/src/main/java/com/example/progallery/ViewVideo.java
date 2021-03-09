e64ec548bd38ed7a58a3d31962592eabf8dc1a66
Hưng Cao <caogiahung11A2@gmail.com>
2021-03-09 20:25:40 +0700

    (+): Add View Video layout
    - Add class ViewVideo
    - Add and design layout

---
+++ app/src/main/java/com/example/progallery/ViewVideo.java
@@ -0,0 +1,13 @@
+package com.example.progallery;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ViewVideo extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.view_video);
+    }
+}
