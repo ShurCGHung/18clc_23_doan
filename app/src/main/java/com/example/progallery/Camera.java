fbf625faad2070e3d9570dc47cd5a6878780796d
Hưng Cao <caogiahung11A2@gmail.com>
2021-03-09 23:38:50 +0700

    (+): Add Camera layout
    Note: Surface View overlap Button

---
+++ app/src/main/java/com/example/progallery/Camera.java
@@ -0,0 +1,13 @@
+package com.example.progallery;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Camera extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.camera);
+    }
+}
