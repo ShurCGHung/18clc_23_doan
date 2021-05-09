fa04c3e291bb656395a44a6b42bb0f7ba0c7482a
Hưng Cao <caogiahung11A2@gmail.com>
2021-05-09 22:53:08 +0700

    (!): Update Settings

---
+++ app/src/main/java/com/example/progallery/view/activities/SettingsActivity.java
@@ -0,0 +1,45 @@
+package com.example.progallery.view.activities;
+
+import android.content.Context;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.preference.PreferenceFragmentCompat;
+
+import com.example.progallery.R;
+import com.example.progallery.helpers.LocaleHelpers;
+
+import org.jetbrains.annotations.NotNull;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.settings_activity);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.settings, new SettingsFragment())
+                    .commit();
+        }
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    public static class SettingsFragment extends PreferenceFragmentCompat {
+        @Override
+        public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+            setPreferencesFromResource(R.xml.root_preferences, rootKey);
+        }
+
+        @Override
+        public void onAttach(@NonNull @NotNull Context context) {
+            super.onAttach(LocaleHelpers.onAttach(context, "en"));
+        }
+    }
+}
